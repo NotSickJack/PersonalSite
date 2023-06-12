05c90eb6d348407226dc0d0984f93d14ab91714c
Your Name <gianluca.prodigo95@gmail.com>
2023-06-12 17:23:06 +0200

    Update scroll

--- src/js/main.js
+++ src/js/main.js
@@ -66,7 +66,7 @@ function Arrow(position) {
     ctx.moveTo(30, 0);
     ctx.lineTo(5, 30);
     ctx.lineWidth = 2;
-    var alpha = 1-(this.dist/175);
+    var alpha = 1-(this.dist/180);
     ctx.strokeStyle = "rgba(255, 255, 255, "+ Math.max(0, alpha) +")";
     ctx.stroke();
     
@@ -98,3 +98,63 @@ function main() {
   }
   requestAnimationFrame(main);
 }
+
+gsap.registerPlugin(ScrollTrigger);
+
+const pageContainer = document.querySelector(".container");
+
+/* SMOOTH SCROLL */
+const scroller = new LocomotiveScroll({
+  el: pageContainer,
+  smooth: true
+});
+
+scroller.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(pageContainer, {
+  scrollTop(value) {
+    return arguments.length
+      ? scroller.scrollTo(value, 0, 0)
+      : scroller.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      left: 0,
+      top: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  },
+  pinType: pageContainer.style.transform ? "transform" : "fixed"
+});
+
+////////////////////////////////////
+////////////////////////////////////
+window.addEventListener("load", function () {
+  let pinBoxes = document.querySelectorAll(".pin-wrap > *");
+  let pinWrap = document.querySelector(".pin-wrap");
+  let pinWrapWidth = pinWrap.offsetWidth;
+  let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+
+  // Pinning and horizontal scrolling
+
+  gsap.to(".pin-wrap", {
+    scrollTrigger: {
+      scroller: pageContainer, //locomotive-scroll
+      scrub: true,
+      trigger: "#sectionPin",
+      pin: true,
+      anticipatePin: 1,
+      start: "top top",
+      end: pinWrapWidth
+    },
+    x: -horizontalScrollLength,
+    ease: "none"
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+
+  ScrollTrigger.refresh();
+});
+
+ScrollTrigger.refresh();
