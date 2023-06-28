a90c8091f2a88dc95eff599a230de36a71dc7bbf
Your Name <gianluca.prodigo95@gmail.com>
2023-06-28 11:41:32 +0200

    update

--- src/js/word.js
+++ src/js/word.js
@@ -1,18 +1,18 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
-import {OrbitControls} from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls";
-
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls";
 
 let scene = new THREE.Scene();
 scene.background = new THREE.Color(0x181818);
 let camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 1, 1000);
-camera.position.set(0, 4, 50);
+camera.position.set(0, 4, 30);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(innerWidth, innerHeight);
 let container = document.getElementById('word');
-container.appendChild(renderer.domElement);window.addEventListener("resize", event => {
+container.appendChild(renderer.domElement);
+
+window.addEventListener("resize", event => {
   camera.aspect = innerWidth / innerHeight;
   camera.updateProjectionMatrix();
- 
 })
 
 let controls = new OrbitControls(camera, renderer.domElement);
@@ -20,56 +20,61 @@ controls.enableDamping = true;
 controls.enablePan = false;
 
 let gu = {
-  time: {value: 0}
-}
+  time: { value: 0 }
+};
 
 let sizes = [];
 let shift = [];
 let pushShift = () => {
   shift.push(
-    Math.random() * Math.PI, 
-    Math.random() * Math.PI * 2, 
+    Math.random() * Math.PI,
+    Math.random() * Math.PI * 2,
     (Math.random() * 0.9 + 0.1) * Math.PI * 0.1,
     Math.random() * 0.9 + 0.1
   );
-}
+};
+
 let pts = new Array(50000).fill().map(p => {
   sizes.push(Math.random() * 1.5 + 0.5);
   pushShift();
   return new THREE.Vector3().randomDirection().multiplyScalar(Math.random() * 0.5 + 9.5);
-})
-for(let i = 0; i < 100000; i++){
+});
+
+for (let i = 0; i < 100000; i++) {
   let r = 10, R = 40;
   let rand = Math.pow(Math.random(), 1.5);
   let radius = Math.sqrt(R * R * rand + (1 - rand) * r * r);
-  pts.push(new THREE.Vector3().setFromCylindricalCoords(radius, Math.random() * 2 * Math.PI, (Math.random() - 0.5) * 2 ));
+  pts.push(new THREE.Vector3().setFromCylindricalCoords(radius, Math.random() * 2 * Math.PI, (Math.random() - 0.5) * 2));
   sizes.push(Math.random() * 1.5 + 0.5);
   pushShift();
 }
 
 let g = new THREE.BufferGeometry().setFromPoints(pts);
 g.setAttribute("sizes", new THREE.Float32BufferAttribute(sizes, 1));
-g.setAttribute("shift", new THREE.Float32BufferAttribute(shift, 4));let m = new THREE.PointsMaterial({
-    size: 0.125,
-    transparent: true,
-    depthTest: false,
-    blending: THREE.AdditiveBlending,
-    onBeforeCompile: shader => {
-      shader.uniforms.time = gu.time;
-      shader.vertexShader = `
-        uniform float time;
-        attribute float sizes;
-        attribute vec4 shift;
-        varying vec3 vColor;
-        ${shader.vertexShader}
-      `.replace(
-        `gl_PointSize = size;`,
-        `gl_PointSize = size * sizes;`
-      ).replace(
-        `#include <color_vertex>`,
-        `#include <color_vertex>
-          vColor = vec3(1.0, 1.0, 1.0); 
-        `
+g.setAttribute("shift", new THREE.Float32BufferAttribute(shift, 4));
+
+let m = new THREE.PointsMaterial({
+  size: 0.125,
+  transparent: true,
+  depthTest: false,
+  blending: THREE.AdditiveBlending,
+  onBeforeCompile: shader => {
+    shader.uniforms.time = gu.time;
+    shader.vertexShader = `
+      uniform float time;
+      attribute float sizes;
+      attribute vec4 shift;
+      varying vec3 vColor;
+      ${shader.vertexShader}
+    `.replace(
+      `gl_PointSize = size;`,
+      `gl_PointSize = size * sizes;`
+    ).replace(
+      `#include <color_vertex>`,
+      `#include <color_vertex>
+        float grayscale = (abs(position.x) + abs(position.y) + abs(position.z)) / 90.0;
+        vColor = vec3(grayscale, grayscale, grayscale);
+      `
     ).replace(
       `#include <begin_vertex>`,
       `#include <begin_vertex>
@@ -92,59 +97,13 @@ g.setAttribute("shift", new THREE.Float32BufferAttribute(shift, 4));let m = new 
       `vec4 diffuseColor = vec4( diffuse, opacity );`,
       `vec4 diffuseColor = vec4( vColor, smoothstep(0.5, 0.1, d)/* * 0.5 + 0.5*/ );`
     );
-    let m = new THREE.PointsMaterial({
-        size: 0.125,
-        transparent: true,
-        depthTest: false,
-        blending: THREE.AdditiveBlending,
-        onBeforeCompile: shader => {
-          shader.uniforms.time = gu.time;
-          shader.vertexShader = `
-            uniform float time;
-            attribute float sizes;
-            attribute vec4 shift;
-            varying vec3 vColor;
-            ${shader.vertexShader}
-          `.replace(
-            `gl_PointSize = size;`,
-            `gl_PointSize = size * sizes;`
-          ).replace(
-            `#include <color_vertex>`,
-            `#include <color_vertex>
-              vColor = vec3(abs(position.x) / 40.0, abs(position.y) / 10.0, abs(position.z) / 40.0);
-            `
-          ).replace(
-            `#include <begin_vertex>`,
-            `#include <begin_vertex>
-              float t = time;
-              float moveT = mod(shift.x + shift.z * t, PI2);
-              float moveS = mod(shift.y + shift.z * t, PI2);
-              transformed += vec3(cos(moveS) * sin(moveT), cos(moveT), sin(moveS) * sin(moveT)) * shift.w;
-            `
-          );
-      
-          shader.fragmentShader = `
-            varying vec3 vColor;
-            ${shader.fragmentShader}
-          `.replace(
-            `#include <clipping_planes_fragment>`,
-            `#include <clipping_planes_fragment>
-              float d = length(gl_PointCoord.xy - 0.5);
-              //if (d > 0.5) discard;
-            `
-          ).replace(
-            `vec4 diffuseColor = vec4( diffuse, opacity );`,
-            `vec4 diffuseColor = vec4( vColor, smoothstep(0.5, 0.1, d) );`
-          );
-        }
-      });
-      
   }
 });
+
 let p = new THREE.Points(g, m);
 p.rotation.order = "ZYX";
 p.rotation.z = 0.2;
-scene.add(p)
+scene.add(p);
 
 let clock = new THREE.Clock();
 
