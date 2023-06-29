327ddcd245ff7eaf63711d605fa09ddbee00c5f8
Your Name <gianluca.prodigo95@gmail.com>
2023-06-29 02:43:17 +0200

    update cursor and loading page

--- src/js/main.js
+++ src/js/main.js
@@ -1,99 +1,35 @@
-document.onmousemove = function (e) {
-  document.body.style.setProperty("--x", e.clientX + "px");
-  document.body.style.setProperty("--y", e.clientY + "px");
-};
+var $circle = $(".cursor"),
+  $follow = $(".cursor-follower");
 
-// hover effect
-const clickableElements = document.querySelectorAll(".clickable");
-clickableElements.forEach((elm) => {
-  elm.addEventListener("mouseover", function () {
-    document.body.classList.add("img-hovered");
+function moveCircle(e) {
+  TweenLite.to($circle, 0.3, {
+    x: e.clientX,
+    y: e.clientY,
   });
-  elm.addEventListener("mouseleave", function () {
-    document.body.classList.remove("img-hovered");
+  TweenLite.to($follow, 0.7, {
+    x: e.clientX,
+    y: e.clientY,
   });
-});
+}
 
-/* var canvas = document.querySelector('.arrows'); */
-/* var ctx = canvas.getContext('2d'); */
-/*  */
-/* canvas.width = window.innerWidth; */
-/* canvas.height = window.innerHeight; */
-/*  */
-/* var mx, my; */
-/*  */
-/* document.addEventListener("mousemove", function(e) { */
-/*     mx = e.clientX; */
-/*     my = e.clientY; */
-/* }, false); */
-/*  */
-/* window.onresize = function() { */
-/*   canvas.width = window.innerWidth; */
-/*   canvas.height = window.innerHeight; */
-/* } */
-/*  */
-/* function Point(x, y) { */
-/*   this.x = x || 0; */
-/*   this.y = y || 0; */
-/* } */
-/*  */
-/* function Arrow(position) { */
-/*   this.pos = position; */
-/*   this.dx = 0; */
-/*   this.dy = 0; */
-/*   this.angle = 0; */
-/*   this.dist = 0; */
-/*    */
-/*   this.update = function() { */
-/*     this.dx = mx - this.pos.x; */
-/*     this.dy = my - this.pos.y; */
-/*     this.dist = Math.sqrt(Math.pow(this.dx, 2) + Math.pow(this.dy, 2)); */
-/*     this.angle = Math.atan2(this.dy, this.dx); */
-/*   } */
-/*    */
-/*   this.draw = function() { */
-/*     ctx.save(); */
-/*     ctx.translate(this.pos.x, this.pos.y); */
-/*     ctx.rotate(this.angle); */
-/*     ctx.beginPath(); */
-/*      */
-/*     ctx.moveTo(30, 0); */
-/*     ctx.lineTo(-30, 0); */
-/*     ctx.moveTo(30, 0); */
-/*     ctx.lineTo(5, -30); */
-/*     ctx.moveTo(30, 0); */
-/*     ctx.lineTo(5, 30); */
-/*     ctx.lineWidth = 2; */
-/*     var alpha = 1-(this.dist / 200); */
-/*     ctx.strokeStyle = "rgba(255, 255, 255, "+ Math.max(0, alpha) +")"; */
-/*     ctx.stroke(); */
-/*      */
-/*     ctx.restore(); */
-/*   } */
-/* } */
-/*  */
-/*  */
-/* var arrowArr = []; */
-/*  */
-/* window.onload = function() { */
-/*   for (var y = 0; y < canvas.height / 20; y++) { */
-/*     for (var x = 0; x < canvas.width / 50; x++) { */
-/*   		var arr = new Arrow(new Point(x * 100, y * 100)); */
-/*        arrowArr.push(arr); */
-/*     } */
-/*   } */
-/*   requestAnimationFrame(main); */
-/* } */
-/*  */
-/* function main() { */
-/*   ctx.clearRect(0, 0, canvas.width, canvas.height); */
-/*    */
-/*   for (var y = 0; y < canvas.height / 20; y++) { */
-/*     for (var x = 0; x < canvas.width / 50; x++) { */
-/*       arrowArr[y * 10 + x].update(); */
-/*       arrowArr[y * 10 + x].draw(); */
-/*     } */
-/*   } */
-/*   requestAnimationFrame(main); */
-/* } */
-/*  */
+function hoverFunc(e) {
+  TweenLite.to($circle, 0.3, {
+    scale: 0,
+  });
+  TweenLite.to($follow, 0.3, {
+    scale: 2,
+  });
+}
+
+function unhoverFunc(e) {
+  TweenLite.to($circle, 0.3, {
+    scale: 1,
+  });
+  TweenLite.to($follow, 0.3, {
+    scale: 1,
+  });
+}
+
+$(window).on("mousemove", moveCircle);
+
+$("a").hover(hoverFunc, unhoverFunc);
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,54 +1,109 @@
-const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-  });
-  
-  function raf(time) {
-    lenis.raf(time);
-    ScrollTrigger.update();
-    requestAnimationFrame(raf);
-  }
-  
-  requestAnimationFrame(raf);
-  
-  const section_1 = document.getElementById("vertical");
-  const col_left = document.querySelector(".col_left");
-  const timeln = gsap.timeline({ paused: true });
-  const mediaQuery = window.matchMedia("(max-width: 900px)");
+var tl = gsap.timeline();
 
-  
-  if (mediaQuery.matches) {
+const welcomeScreen = gsap.timeline({
+  paused: "true",
+});
+const fromLeft = document.querySelector("transition-container");
+
+tl.from("#loader", {
+  duration: 0.3,
+  opacity: 0,
+  y: 10,
+});
+
+let tl1 = gsap.timeline({ ease: "power4.inOut", paused: "true" });
+
+tl1.set(fromLeft, { pointerEvents: "none" });
+tl1.to(".from-left .tile", {
+  duration: 0.6,
+  width: "100%",
+  left: "0%",
+
+});
+tl1.to(".from-left .tile", {
+  duration: 0.5,
+  width: "100%",
+  left: "100%",
+  stagger: 0.1,
+});
+tl1.set(".from-left .tile", { left: "0", width: "0" });
+tl1.set(fromLeft, { pointerEvents: "all" });
+
+let id,
+  i = 0;
+function loader() {
+  id = setInterval(frame, 20);
+}
+function frame() {
+  if (i >= 100) {
+    clearInterval(id);
+    tl1.play();
+    welcomeScreen.play();
   } else {
-    timeln.fromTo(col_left, { y: 0 }, { y: '30vh', duration: 2, ease: 'yes' }, 0);
-  }  
-  ScrollTrigger.config({ 
-    limitCallbacks: true,
-    ignoreMobileResize: true
-  });
-  
-  const scroll_1 = ScrollTrigger.create({
-    animation: timeln,
-    trigger: section_1,
-    start: 'top top',
-    end: 'bottom center',
-    scrub: true
-  });
-  
-  const section_2 = document.getElementById("horizontal");
-  let box_items = gsap.utils.toArray(".horizontal__item");
-  
-  
-  if (mediaQuery.matches) {
-    section_2.style.overflowX = "auto";
-  } else {
-    gsap.to(box_items, {
-      xPercent: -100 * (box_items.length - 1),
-      ease: "sine.out",
-      scrollTrigger: {
-        trigger: section_2,
-        pin: true,
-        scrub: 1,
-        end: "+=" + section_2.offsetWidth
-      }
-    });
+    i++;
+    document.getElementById("loader").innerHTML = i + "%";
   }
+}
+window.onload = function () {
+  loader();
+};
+
+
+welcomeScreen.to(".loading-screen", {
+  duration: 0.8,
+  y: -2000,
+  ease: "Power4.out",
+  delay: 0.4,
+});
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+});
+
+function raf(time) {
+  lenis.raf(time);
+  ScrollTrigger.update();
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+const section_1 = document.getElementById("vertical");
+const col_left = document.querySelector(".col_left");
+const timeln = gsap.timeline({ paused: true });
+const mediaQuery = window.matchMedia("(max-width: 900px)");
+
+if (mediaQuery.matches) {
+} else {
+  timeln.fromTo(col_left, { y: 0 }, { y: "30vh", duration: 2, ease: "yes" }, 0);
+}
+ScrollTrigger.config({
+  limitCallbacks: true,
+  ignoreMobileResize: true,
+});
+
+const scroll_1 = ScrollTrigger.create({
+  animation: timeln,
+  trigger: section_1,
+  start: "top top",
+  end: "bottom center",
+  scrub: true,
+});
+
+const section_2 = document.getElementById("horizontal");
+let box_items = gsap.utils.toArray(".horizontal__item");
+
+if (mediaQuery.matches) {
+  section_2.style.overflowX = "auto";
+} else {
+  gsap.to(box_items, {
+    xPercent: -100 * (box_items.length - 1),
+    ease: "sine.out",
+    scrollTrigger: {
+      trigger: section_2,
+      pin: true,
+      scrub: 1,
+      end: "+=" + section_2.offsetWidth,
+    },
+  });
+}
